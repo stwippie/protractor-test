ea2266242018521a521fb7d0737d57ecdbadef76
Stwippie <yulishik@gmail.com>
2016-12-25 16:39:25 +0300

    init commit

---
+++ conf.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Stwippie on 25/12/2016.
+ */
+// conf.js
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js']
+};
