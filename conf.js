f833785d225a06ac2684a681d228e2ca411a6c17
Stwippie <yulishik@gmail.com>
2016-12-25 16:50:41 +0300

    Update conf.js

--- conf.js
+++ conf.js
@@ -5,5 +5,8 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js']
+  specs: ['spec.js'],
+  capabilities: {
+    'browserName': 'phantomjs',
+  }
 };
